2cb3a252978cf82639b0ca25491d5531ee21b602
mnaibei <muchajulius@gmail.com>
2023-09-26 09:32:02 +0300

    Add controller actions to project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
-# class UsersController < ApplicationController
-#   def index
-#     @users = User.all
+class UsersController < ApplicationController
+  def index
+    @users = User.all
 
-#     if @users
-#       render json: @users
-#     else
-#       render json: {
-#         status: 500,
-#         errors: ['No users found']
-#       }
-#     end
-#   end
-# end
+    if @users
+      render json: @users
+    else
+      render json: {
+        status: 500,
+        errors: ['No users found']
+      }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
-# Rails.application.routes.draw do
-#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-#   # Defines the root path route ("/")
-#   # root "articles#index"
+  # Defines the root path route ("/")
+  # root "articles#index"
 
-#   resources :landmarks, only: %i[index show]
-# end
+  resources :landmarks, only: %i[index show]
+end
