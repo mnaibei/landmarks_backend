aac4cd2d0b55cf86fc6c7329b99939fbb7f6658f
mnaibei <muchajulius@gmail.com>
2023-09-26 17:11:01 +0300

    Add rescue clause to controllers

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -2,26 +2,22 @@ class LandmarksController < ApplicationController
   def index
     @landmarks = Landmark.all
 
-    if @landmarks
-      render json: @landmarks, status: :ok, pretty: true
-    else
-      render json: {
-        status: 500,
-        errors: ['No landmarks found']
-      }
-    end
+    render json: @landmarks, status: :ok, pretty: true
+  rescue ActiveRecord::RecordNotFound
+    render json: {
+      status: 500,
+      errors: ['No landmarks found']
+    }
   end
 
   def show
     @landmark = Landmark.find(params[:id])
 
-    if @landmark
-      render json: @landmark, status: :ok, pretty: true
-    else
-      render json: {
-        status: 500,
-        errors: ['Landmark not found']
-      }
-    end
+    render json: @landmark, status: :ok, pretty: true
+  rescue ActiveRecord::RecordNotFound
+    render json: {
+      status: 404, # Not Found
+      errors: ['Landmark not found']
+    }
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,11 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    if @users
-      render json: @users
-    else
-      render json: {
-        status: 500,
-        errors: ['No users found']
-      }
-    end
+    render json: @users
+  rescue ActiveRecord::RecordNotFound
+    render json: {
+      status: 500,
+      errors: ['No users found']
+    }
   end
 end
