fc9184e1d3618797ac01de4e184ca47b5ccb6417
mnaibei <muchajulius@gmail.com>
2023-09-26 18:11:16 +0300

    Add user_reviews controller and modify routes.rb

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,18 +1,19 @@
 class LandmarksController < ApplicationController
   def index
-    @landmarks = Landmark.all
+    @landmarks = Landmark.all.includes(:user_reviews)
 
-    render json: @landmarks, status: :ok, pretty: true
-  rescue ActiveRecord::RecordNotFound
-    render json: {
-      status: 500,
-      errors: ['No landmarks found']
-    }
+    if @landmarks.any?
+      render json: @landmarks, status: :ok, pretty: true
+    else
+      render json: {
+        status: 500,
+        errors: ['No landmarks found']
+      }
+    end
   end
 
   def show
-    @landmark = Landmark.find(params[:id])
-
+    @landmark = Landmark.includes(:user_reviews).find(params[:id])
     render json: @landmark, status: :ok, pretty: true
   rescue ActiveRecord::RecordNotFound
     render json: {
